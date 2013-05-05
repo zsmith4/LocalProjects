0f53f51079e68801733fd375c8eafe613e55d118
Zach <zsmith4@hotmail.com>
2013-05-05 16:53:16 -0600

    Added DatesWithDashes

---
+++ DatesWithDashes/DatesWithDashes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace DatesWithDashes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string path = args[0];
+			//string path = @"C:\Users\Zach\Photos";
+
+			if (String.IsNullOrEmpty(path))
+			{
+				Console.WriteLine("Please provide a path as the first argument");
+				return;
+			}
+			var source = new DirectoryInfo(path);
+			
+			var allDirs = source.GetDirectories("*", SearchOption.AllDirectories);
+			foreach (var subDir in allDirs)
+			{
+				string newName = AddDashesToDateInDirName(subDir.Name);
+				var di = new DirectoryInfo(Path.Combine(subDir.Parent.FullName, newName));
+
+				if (di.FullName != subDir.FullName)
+				{
+					subDir.MoveTo(di.FullName);
+				}
+			}
+		}
+
+		public static string AddDashesToDateInDirName(string dirName)
+		{
+			//define pattern and replacement
+			string pattern = @"(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})(?<remain>\b.*)";
+			string replacement = "${year}-${month}-${day}${remain}";
+
+			//perform replacement
+			Regex regEx = new Regex(pattern);
+			string result = regEx.Replace(dirName, replacement);
+
+			if (!String.IsNullOrEmpty(result))
+			{
+				return result;
+			}
+
+			return dirName;
+		}
+	}
+}
